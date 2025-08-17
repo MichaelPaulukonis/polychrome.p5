750d43df3fb4e995f9d20a202c7056d38e258f01
Michael Paulukonis <michael.paulukonis@gmail.com>
2025-08-16 23:10:56 -0400

    feat: Migrate nuxt.config.js to nuxt.config.ts for Nuxt 3

---
+++ nuxt.config.ts
@@ -0,0 +1,58 @@
+import { defineNuxtConfig } from 'nuxt/config'
+import { version } from './package.json'
+
+// only add `router.base = '/<repository-name>/'` if `DEPLOY_ENV` is `GH_PAGES`
+const routerBase = process.env.DEPLOY_ENV === 'GH_PAGES' ? '/polychrome.p5/' : ''
+
+export default defineNuxtConfig({
+  ssr: false,
+  runtimeConfig: {
+    public: {
+      appVersion: version
+    }
+  },
+  app: {
+    baseURL: routerBase,
+    head: {
+      title: process.env.npm_package_name || '',
+      meta: [
+        { charset: 'utf-8' },
+        { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+        { hid: 'description', name: 'description', content: process.env.npm_package_description || '' }
+      ],
+      link: [
+        { rel: 'icon', type: 'image/x-icon', href: `${routerBase.replace(/\/$/, '')}/favicon.ico` }
+      ]
+    }
+  },
+  /*
+  ** Customize the progress-bar color
+  */
+  loading: { color: '#fff' },
+  /*
+  ** Global CSS
+  */
+  css: [
+    '@/assets/css/main.scss',
+    '@/assets/css/core.css'
+  ],
+  /*
+  ** Plugins to load before mounting the App
+  */
+  plugins: [
+    { src: '~/plugins/setFocus', mode: 'client' }
+  ],
+  /*
+  ** Nuxt.js modules
+  */
+  modules: [
+    'nuxt-webfontloader'
+  ],
+
+  webfontloader: {
+    custom: {
+      families: ['Graphik', 'Tiempos Headline'],
+      urls: [`${routerBase}/fonts/fonts.css`]
+    }
+  },
+})
